ed633f49bba6e7b2a1575b49d25ec4a7d805d2b9
Tobzy <oluwatjtunjex@gmail.com>
2025-06-23 01:55:06 +0100

    Fix CORS for frontend access

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require ('express')
 const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors')
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:5174',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true 
+}));
 app.use(express.json({limit:'50mb'}))
 const bcrypt = require('bcryptjs')
 const ejs = require('ejs')
