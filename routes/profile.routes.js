e71eb9557834e8ef847fae89c0ca9f75b2898565
Tobzy <oluwatjtunjex@gmail.com>
2025-07-18 02:03:05 +0100

    edited pfile function

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
-const verifyToken = require('../middleware/verifyToken');
+const User = require('../models/user.model');
+const verifyToken = require('../middleware/auth');
 
 // GET /api/profile
 router.get('/profile', verifyToken, async (req, res) => {
